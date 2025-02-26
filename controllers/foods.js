db724fc89afa8b363d3e19c43bb8bf6f71091d60
amenalii <ummeamen@gmail.com>
2025-02-26 01:33:02 -0600

    Added Delete, Edit & Update Routes

--- controllers/foods.js
+++ controllers/foods.js
@@ -24,8 +24,31 @@ router.get("/new", async (req, res) => {
 });
 
 // Delete Route
+router.delete('/:itemId', async (req, res) => {
+  try {
+      const currentUser = await User.findById(req.session.user._id);
+      currentUser.pantry.id(req.params.itemId).deleteOne();
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+      console.log(error);
+      res.redirect('/');
+  }
+});
 
 // Update Route
+router.put('/:itemId', async (req, res) => {
+  try {
+      const currentUser = await User.findById(req.session.user._id);
+      const pantryItem = currentUser.pantry.id(req.params.itemId);
+      pantryItem.set(req.body);
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+      console.log(error);
+      res.redirect('/');
+  }
+});
 
 // Create Route
 router.post("/", async (req, res) => {
@@ -41,7 +64,15 @@ router.post("/", async (req, res) => {
 });
 
 // Edit Route
-
-// Show Route
+router.get('/:itemId/edit', async (req, res) => {
+  try {
+      const currentUser = await User.findById(req.session.user._id);
+      const pantryItem = currentUser.pantry.id(req.params.itemId);
+      res.render(`foods/edit.ejs`, { pantry: pantryItem });
+  } catch (error) {
+      console.log(error);
+      res.redirect('/');
+  }
+});
 
 module.exports = router;
